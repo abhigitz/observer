212446c55d6b3278e7eea195d1f5d582fefa58b0
Ashish Anand <ashishthedev@gmail.com>
2014-09-19 15:52:14 +0530

    Features and landing pages are now declarative

--- main.py
+++ main.py
@@ -35,61 +35,86 @@ def SendDeferredMailToAdmins(subject, body, htmlBody=None):
 
 def SendInstantMailToAdmins(subject, body, htmlBody=None):
     mail.send_mail_to_admins(SA[::-1], subject, body, html=htmlBody)
+    return
+
+features = [
+    {
+      "featureName": "paymentPage",
+      "servingUrl": "/pmt",
+      "templatePath": "templates/pmt.html",
+      "apiPath": "/api/get-outstanding-pmt-data",
+      "methods": ["POST"],
+      "jsonPath": 'static/dbs/pmt.json',
+      },
+
+    {
+      "featureName": "PendingFormC",
+      "servingUrl": "/formC",
+      "templatePath": "templates/formC.html",
+      "apiPath": "/api/get-formC-data",
+      "methods": ["POST"],
+      "jsonPath": 'static/dbs/formC.json',
+      },
+
+    {
+      "featureName": "orders",
+      "servingUrl": "/order",
+      "templatePath": "templates/order.html",
+      "apiPath": "/api/get-pending-orders-data",
+      "methods": ["POST"],
+      "jsonPath": 'static/dbs/order.json',
+      },
+
+    {
+      "featureName": "RawMaterial",
+      "servingUrl": "/rawmat",
+      "templatePath": "templates/rawmat.html",
+      "apiPath": "/api/get-rawmaterial-data",
+      "methods": ["POST"],
+      "jsonPath": 'static/dbs/rawmat.json',
+      },
+
+    ]
+
+def create_handler(featureName, servingUrl, templatePath):
+  def handler(featureName, templatePath):
+    AuthorizeAndInform(users.get_current_user(), featureName)
+    return make_response(open(templatePath).read())
+  decorator = app.route(servingUrl)
+  from functools import partial
+
+  p = partial(handler, featureName=featureName, templatePath=templatePath)
+  p.__name__ = "func_{}".format(featureName)
+
+  return decorator(p)
+
+
+
+def create_api_server(featureName, apiPath, methods, jsonPath):
+  def api_server(jsonPath):
+    return make_response(open(jsonPath).read())
+  decorator = app.route(apiPath, methods=methods)
+  from functools import partial
+
+  p = partial(api_server, jsonPath=jsonPath)
+  p.__name__ = "api_{}".format(featureName)
+
+  return decorator(p)
+
+def GenrateAllHandlersAndAPIServers():
+  for f in features:
+    #All the features have a similar structure, there is and html page and there is an api end point which is referred to retrieve the whole dataset. So we have defined all the 
+    create_handler(f["featureName"], f["servingUrl"], f["templatePath"])
+    create_api_server(f["featureName"], f["apiPath"], f["methods"], f["jsonPath"])
+
 
 @app.route('/_ah/warmup')
 def WarmupPage():
     return make_response("")
 
-@app.route('/order')
-def orderPage():
-    """Payment page of the application."""
-    AuthorizeAndInform(users.get_current_user(), "Order")
-    return make_response(open('templates/order.html').read())
 
-
-@app.route('/pmt')
-def paymentPage():
-    """Payment page of the application."""
-    AuthorizeAndInform(users.get_current_user(), "PMT")
-    return make_response(open('templates/pmt.html').read())
-
-@app.route('/formC')
-def PendingFormCPage():
-    """Pending Form-C"""
-    AuthorizeAndInform(users.get_current_user(), "FORMC")
-    return make_response(open('templates/formC.html').read())
-
-@app.route('/rawmat')
-def RawMaterialAsOfNowPage():
-    """Raw material as of now"""
-    AuthorizeAndInform(users.get_current_user(), "RawMaterial")
-    return make_response(open('templates/rawmat.html').read())
-
-@app.route('/')
-def indexPage():
-    """First Page of the application."""
-    AuthorizeAndInform(users.get_current_user(), "Main")
-    return make_response(open('templates/index.html').read())
-
-@app.route('/api/get-pending-orders-data', methods=['POST'])
-def api_get_order_data():
-    return make_response(open('static/dbs/order.json').read())
-
-@app.route('/api/get-outstanding-pmt-data', methods=['POST'])
-def api_get_pmt_data():
-    return make_response(open('static/dbs/pmt.json').read())
-
-@app.route('/api/get-km-pending-po-data', methods=['POST'])
-def api_get_kmpo_data():
-    return make_response(open('static/dbs/kmOrder.json').read())
-
-@app.route('/api/get-formC-data', methods=['POST'])
-def api_get_formc_data():
-    return make_response(open('static/dbs/formC.json').read())
-
-@app.route('/api/get-rawmaterial-data', methods=['POST'])
-def api_get_rawmat_data():
-    return make_response(open('static/dbs/rawmat.json').read())
+GenrateAllHandlersAndAPIServers()
+create_handler("indexPage", "/", "templates/index.html")
 
 
 @app.errorhandler(404)
